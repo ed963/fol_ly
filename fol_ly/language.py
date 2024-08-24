fd37a5e51157af04aeaf64cce82deb1312143ce1
E J <64994681+ed963@users.noreply.github.com>
2024-08-24 00:50:27 -0400

    Implement Language class and test cases.

---
+++ fol_ly/language.py
@@ -0,0 +1,192 @@
+from __future__ import annotations
+
+import re
+
+
+"""This module provides an implementation of first-order languages (which
+will be referred to as simply "languages").
+
+Languages are collections of "symbols". In our implementation, a "symbol"
+will be a finite, non-empty string that contains no whitespace. For example,
+"+", "0", "v1" would be considered valid symbols. 
+
+In our implementation, a "string" of a language is a space-delimited sequence
+of symbols. Combining this with the requirement that symbols contain no 
+whitespace allows for strings to be properly parsed. For example, 
+"( AA v1 ) ( = v1 v2 )" would be a string of 9 symbols.
+
+The symbols of a language can be divided into "logical symbols" and 
+"non-logical symbols":
+    - Logical symbols are common to all languages. 
+    - Non-logical symbols are the constant symbols, functions symbols, and 
+    relation symbols that are defined for each particular language.
+"""
+
+
+# The set of logical symbols common to all first order languages.
+#   To limit our character set to ASCII, we use:
+#       "||" to represent the disjunction symbol (U+2228)
+#       "!!" to represent the negation symbol (U+00AC)
+#       "AA" to represent the universal quantification symbol (U+2200)
+COMMON_SYMBOLS = {"(", ")", "||", "!!", "AA", "="}
+# We also use "v1, v2, ..." to represent the variable symbols of all languages
+VAR_SYMBOL_RE = r"^v[1-9]\d*$"
+
+
+def is_disjoint_sets(*sets: set) -> bool:
+    """Return whether the given sets are pairwise disjoint."""
+    all_elements = set()
+    for s in sets:
+        for element in s:
+            if element in all_elements:
+                return False
+        all_elements.update(s)
+    return True
+
+
+class Language:
+    """A language of first order logic."""
+
+    # The constant symbols of this language
+    _constants: set[str]
+    # The function symbols of this language
+    _functions: dict[int, set[str]]
+    # The relation symbols of this language
+    _relations: dict[int, set[str]]
+
+    def __init__(
+        self, constants: set, functions: dict[int:set], relations: dict[int:set]
+    ):
+        """Initialize this language with the given constant, function, and
+        relation symbols.
+
+        Each set of symbols must be pairwise disjoint, and also cannot
+        include any of the COMMON_SYMBOLS.
+
+        Args:
+            constants: A set of strings representing the constant symbols of
+                this language.
+            functions: A dictionary mapping positive integers to sets
+                of function symbols. The key represents the arity of
+                the function symbols in the set.
+            relations: A dictionary mapping positive integers to sets
+                of relation symbols. The key represents the arity of
+                the relation symbols in the set.
+
+        Raises:
+            ValueError: If any of the dictionaries are ill-formed, or
+                any strings in the sets cannot be used as a constant,
+                function, or relation symbol.
+        """
+        if not is_disjoint_sets(constants, *functions.values(), *relations.values()):
+            raise ValueError("A symbol appears in more than one set.")
+
+        if not all(
+            {Language.is_valid_nonlogical_symbol(symbol) for symbol in constants}
+        ):
+            raise ValueError("Invalid constant symbol.")
+
+        if not all({isinstance(key, int) and key > 0 for key in functions}):
+            raise ValueError("Invalid key in functions.")
+        for symbol_set in functions.values():
+            if not all(
+                {Language.is_valid_nonlogical_symbol(symbol) for symbol in symbol_set}
+            ):
+                raise ValueError("Invalid function symbol.")
+
+        if not all({isinstance(key, int) and key > 0 for key in relations}):
+            raise ValueError("Invalid key in relations.")
+        for symbol_set in relations.values():
+            if not all(
+                {Language.is_valid_nonlogical_symbol(symbol) for symbol in symbol_set}
+            ):
+                raise ValueError("Invalid relation symbol.")
+
+        self._constants = constants.copy()
+        self._functions = {n: functions[n].copy() for n in functions}
+        self._relations = {n: relations[n].copy() for n in relations}
+
+    def __repr__(self):
+        return "Language(constants=%r,functions=%r,relations=%r)" % (
+            self._constants,
+            self._functions,
+            self._relations,
+        )
+
+    def __str__(self):
+        function_symbols = [
+            f"\t{n}-ary: {self._functions[n]}\n" for n in sorted(self._functions.keys())
+        ]
+        relation_symbols = [
+            f"\t{n}-ary: {self._relations[n]}\n" for n in sorted(self._relations.keys())
+        ]
+        return (
+            f"-Language-\n"
+            f"Constants: {self._constants}\n"
+            f"Functions:\n"
+            f"{''.join(function_symbols)}"
+            f"Relations:\n"
+            f"{''.join(relation_symbols)}"
+        )
+
+    def __eq__(self, other: Language):
+        return (
+            self._constants == other._constants
+            and set(self._functions.keys()) == set(other._functions.keys())
+            and set(self._relations.keys()) == set(other._relations.keys())
+            and all(
+                {self._functions[n] == other._functions[n] for n in self._functions}
+            )
+            and all(
+                {self._relations[n] == other._relations[n] for n in self._relations}
+            )
+        )
+
+    def is_valid_nonlogical_symbol(symbol: str) -> bool:
+        """Return whether the given string can be used as a constant, function,
+        or relation symbol of a first order language.
+        """
+        return (
+            isinstance(symbol, str)
+            and symbol not in COMMON_SYMBOLS
+            and re.search(r"\s", symbol) is None
+            and not Language.is_variable_symbol(symbol)
+        )
+
+    def is_common_symbol(symbol: str) -> bool:
+        """Return whether the given symbol is one of the symbols common
+        to all first order languages.
+        """
+        return symbol in COMMON_SYMBOLS
+
+    def is_variable_symbol(symbol: str) -> bool:
+        """Return whether the given symbol is a variable symbol."""
+        return re.match(VAR_SYMBOL_RE, symbol)
+
+    def is_constant_symbol(self, symbol: str) -> bool:
+        """Return whether the given symbol is a constant symbol
+        of this language.
+        """
+        return symbol in self._constants
+
+    def get_function_arity(self, symbol: str) -> int | None:
+        """Return the arity of the given function symbol, or None if the given
+        symbol is not a function symbol of this language.
+        """
+        for n in self._functions:
+            if symbol in self._functions[n]:
+                return n
+
+    def get_relation_arity(self, symbol: str) -> int | None:
+        """Return the arity of the given relation symbol, or None if the given
+        symbol is not a relation symbol of this language.
+        """
+        for n in self._relations:
+            if symbol in self._relations[n]:
+                return n
+
+    def get_variable_index(variable: str) -> int:
+        """Given a variable (i.e., a string of the form "vn"), return the int n."""
+        if not Language.is_variable_symbol(variable):
+            raise ValueError(f"Not a variable: {variable}")
+        return int(variable[1:])
---
+++ test/test_language.py
@@ -0,0 +1,110 @@
+import unittest
+
+from fol_ly.language import Language, is_disjoint_sets
+
+
+class TestIsDisjointSets(unittest.TestCase):
+    def test_is_disjoint_sets_false(self):
+        self.assertFalse(is_disjoint_sets({"a", "b", "c"}, {"c", "d", "e"}))
+        self.assertFalse(is_disjoint_sets({"a"}, {"c", "d", "e"}, {"d"}, {"f"}))
+
+    def test_is_disjoint_sets_true(self):
+        self.assertTrue(is_disjoint_sets({"a", "b", "c"}, {"x", "y", "z"}))
+        self.assertTrue(is_disjoint_sets({"a"}, {"c", "d", "e"}, {"f"}, {"g"}))
+
+
+class TestLanguage(unittest.TestCase):
+    def test_init_sunny_empty(self):
+        l = Language(set(), {}, {})
+        self.assertEqual(l._constants, set())
+        self.assertEqual(l._functions, {})
+        self.assertEqual(l._relations, {})
+
+    def test_init_sunny_compex(self):
+        l = Language({"a", "b", "pi"}, {1: {"f1", "f2"}, 3: {"f3"}}, {2: {"r1"}})
+        self.assertEqual(l._constants, {"a", "b", "pi"})
+        self.assertEqual(l._functions, {1: {"f1", "f2"}, 3: {"f3"}})
+        self.assertEqual(l._relations, {2: {"r1"}})
+
+    def test_init_rainy_invalid_symbol(self):
+        with self.assertRaises(ValueError):
+            Language({"v1"}, {}, {})
+
+        with self.assertRaises(ValueError):
+            Language(set(), {1: {"||"}}, {})
+
+        with self.assertRaises(ValueError):
+            Language(set(), {}, {2: {"r 2"}})
+
+    def test_init_rainy_repeat_symbol(self):
+        with self.assertRaises(ValueError):
+            Language({"a"}, {1: {"a"}}, {})
+
+    def test_eq_true(self):
+        l1 = Language({"a", "b", "pi"}, {1: {"f1", "f2"}, 3: {"f3"}}, {2: {"r1"}})
+        l2 = Language({"a", "b", "pi"}, {1: {"f1", "f2"}, 3: {"f3"}}, {2: {"r1"}})
+        self.assertEqual(l1, l1)
+        self.assertEqual(l1, l2)
+
+    def test_eq_false(self):
+        l1 = Language({"a", "b", "pi"}, {1: {"f1", "f2"}, 3: {"f3"}}, {2: {"r1"}})
+        l2 = Language({"a", "b"}, {1: {"f1", "f2"}, 3: {"f3"}}, {2: {"r1"}})
+        l3 = Language({"a", "b", "pi"}, {3: {"f1", "f2"}, 1: {"f3"}}, {2: {"r1"}})
+        l4 = Language({"a", "b", "pi"}, {3: {"f1", "f2"}, 1: {"f3"}}, {})
+        self.assertNotEqual(l1, l2)
+        self.assertNotEqual(l1, l3)
+        self.assertNotEqual(l1, l4)
+
+    def test_is_valid_non_logical_symbol_false(self):
+        self.assertFalse(Language.is_valid_nonlogical_symbol(3.14))
+        self.assertFalse(Language.is_valid_nonlogical_symbol(["x"]))
+        self.assertFalse(Language.is_valid_nonlogical_symbol("AA"))
+        self.assertFalse(Language.is_valid_nonlogical_symbol("("))
+        self.assertFalse(Language.is_valid_nonlogical_symbol("A 2"))
+        self.assertFalse(Language.is_valid_nonlogical_symbol("v314"))
+
+    def test_is_valid_non_logical_symbol_true(self):
+        self.assertTrue(Language.is_valid_nonlogical_symbol("A"))
+        self.assertTrue(Language.is_valid_nonlogical_symbol("x2"))
+
+    def test_is_variable_symbol_true(self):
+        self.assertTrue(Language.is_variable_symbol("v1"))
+        self.assertTrue(Language.is_variable_symbol("v1111"))
+
+    def test_is_variable_symbol_false(self):
+        self.assertFalse(Language.is_variable_symbol("v"))
+        self.assertFalse(Language.is_variable_symbol("1v"))
+        self.assertFalse(Language.is_variable_symbol("v 2"))
+        self.assertFalse(Language.is_variable_symbol("v 0"))
+
+    def test_get_function_arity_symbol_exists(self):
+        l = Language({"a", "b", "pi"}, {1: {"f1", "f2"}, 3: {"f3"}}, {2: {"r1"}})
+        self.assertEqual(l.get_function_arity("f1"), 1)
+        self.assertEqual(l.get_function_arity("f3"), 3)
+
+    def test_get_function_arity_none(self):
+        l = Language({"a", "b", "pi"}, {1: {"f1", "f2"}, 3: {"f3"}}, {2: {"r1"}})
+        self.assertIsNone(l.get_function_arity("f5"))
+        self.assertIsNone(l.get_function_arity("r1"))
+        self.assertIsNone(l.get_function_arity("pi"))
+
+    def test_get_relation_arity_symbol_exists(self):
+        l = Language(
+            {"a", "b", "pi"}, {1: {"f1", "f2"}, 3: {"f3"}}, {2: {"r1"}, 10: {"r10"}}
+        )
+        self.assertEqual(l.get_relation_arity("r1"), 2)
+        self.assertEqual(l.get_relation_arity("r10"), 10)
+
+    def test_get_relation_arity_none(self):
+        l = Language({"a", "b", "pi"}, {1: {"f1", "f2"}, 3: {"f3"}}, {2: {"r1"}})
+        self.assertIsNone(l.get_relation_arity("f1"))
+        self.assertIsNone(l.get_relation_arity("r2"))
+        self.assertIsNone(l.get_relation_arity("a"))
+
+    def test_get_variable_index(self):
+        self.assertEqual(Language.get_variable_index("v1"), 1)
+        self.assertEqual(Language.get_variable_index("v345"), 345)
+
+
+if __name__ == "__main__":
+    unittest.main()
